8bc89e5261ff22970d78b89dc702d11951711ed6
CaptMerica <rmerica@live.com>
2023-02-06 14:13:40 -0500

    update questionDetails

--- src/components/QuestionDetails/QuestionDetails.jsx
+++ src/components/QuestionDetails/QuestionDetails.jsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { show } from "../../services/questionService"
 
+const QuestionDetails = () => {
+  const [ questionDetails, setQuestionDetails ] = useState({})
+  const { id } = useParams()
 
-const QuestionDetails = ({questions}) => {
-  console.log(questions)
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const questionData = await show (id)
+      setQuestionDetails(questionData)
+    }
+    fetchDetails()
+  }, [id])
+
   return (
     <>
     <section>
       <div>
         <h2>
-          {questions.name}
+          {questionDetails.name}
         </h2>
       </div>
     </section>
