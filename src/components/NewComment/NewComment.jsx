b1703bcb714a12f3ed92cb278f8c836ac4503557
Oscar Nunez <requinto31@gmail.com>
2023-02-06 22:43:13 -0500

    stub up NewComment function in NewComment component

--- src/components/NewComment/NewComment.jsx
+++ src/components/NewComment/NewComment.jsx
@@ -1,2 +1,15 @@
 import { useState } from "react";
 import styles from './NewComment.module.css'
+
+const NewComment = (props) => {
+
+
+  return (
+    <>
+    
+    </>
+  )
+
+}
+
+export default NewComment
