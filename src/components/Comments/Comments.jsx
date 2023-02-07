928c9fb0d9e86afe811e9e650e5491299b148b0c
CaptMerica <rmerica@live.com>
2023-02-07 10:37:47 -0500

    create Comments function

---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,19 @@
+import CommentCard from '../CommentCard/CommentCard'
+
+const Comments = (props) => {
+  if (!props.comments) return <h4>No Comments</h4>
+
+  return (
+    <>
+      {props.comments.map((comment) => (
+        <CommentCard
+          key={comment._id}
+          comment={comment}
+          user={props.user}
+        />
+      ))}
+    </>
+  )
+}
+
+export default Comments
