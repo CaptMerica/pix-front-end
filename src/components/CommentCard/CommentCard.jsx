a2d7ab9bc36be92445039857994d562f3e6f9dd9
CaptMerica <rmerica@live.com>
2023-02-07 10:36:07 -0500

    create comments and commentscard

---
+++ src/components/CommentCard/CommentCard.jsx
@@ -0,0 +1,14 @@
+import AuthorInfo from "../AuthorInfo/AuthorInfo"
+
+const CommentCard = ({ comment }) => {
+  return (
+    <article>
+      <header>
+        <AuthorInfo content={comment} />
+      </header>
+      <p>{comment.text}</p>
+    </article>
+  )
+}
+
+export default CommentCard
