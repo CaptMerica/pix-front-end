0e3ba74e29cb33eae41b147ecdda5cf0b156a0ec
Oscar Nunez <requinto31@gmail.com>
2023-02-07 11:25:31 -0500

    implement index service function

--- src/services/quoteService.js
+++ src/services/quoteService.js
@@ -2,14 +2,13 @@ import * as tokenService from './tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/quotes`
 
+// https://localhost:3001/quotes
 const index = async () => {
   try {
-    const res = await fetch(BASE_URL, {
-      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
-    })
+    const res = await fetch(BASE_URL)
     return res.json()
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
