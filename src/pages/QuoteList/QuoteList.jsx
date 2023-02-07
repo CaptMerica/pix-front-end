1cf9c35aea48cc97ab500985f429f3cd8f578d72
Oscar Nunez <requinto31@gmail.com>
2023-02-07 12:40:02 -0500

    place NewQUote form in QuotesList.jsx

--- src/pages/QuoteList/QuoteList.jsx
+++ src/pages/QuoteList/QuoteList.jsx
@@ -1,9 +1,11 @@
 import QuoteCard from "../../components/QuoteCard/QuoteCard"
+import NewQuote from "../NewQuote/NewQuote"
 
 const QuoteList = (props) => {
   console.log(props)
   return (
     <>
+      <NewQuote to="quotes/new">New Quote</NewQuote>
       {props.quotes.map((quote) => (
         <div key={quote._id}>
           <QuoteCard quote={quote} />
