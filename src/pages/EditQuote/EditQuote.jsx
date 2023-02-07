40fa50560374483afe3b26e9954fd32d7864b262
Oscar Nunez <requinto31@gmail.com>
2023-02-07 15:22:53 -0500

    build out EditQuote.jsx

---
+++ src/pages/EditQuote/EditQuote.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import { useLocation } from "react-router-dom"
+import styles from './EditQuote.module.css'
+
+const EditQuote = (props) => {
+  const { state } = useLocation()
+  const [form, setForm] = useState(state)
+
+  console.log(state)
+  const handleChange = ({ target }) => {
+    setForm({ ...form, [target.name]: target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Update this line shortly...
+  }
+
+  return (
+    <>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            required
+            type="text"
+            name="content"
+            id="content-input"
+            value={form.content}
+            placeholder="Edit Quote"
+            onChange={handleChange}
+          />
+          <button type="submit">Edit Quote</button>
+        </form>
+      </main>
+    </>
+  )
+}
+
+export default EditQuote
