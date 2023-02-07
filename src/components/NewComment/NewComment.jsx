43331add13dd74906ad77c3a9fc7ecbb85cd35cc
Patrick Mazara <patrickmazara@hotmail.com>
2023-02-07 14:29:32 -0500

    complete NewQuestion component form and functionality

--- src/components/NewComment/NewComment.jsx
+++ src/components/NewComment/NewComment.jsx
@@ -11,7 +11,6 @@ const NewComment = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     props.handleAddComment(form)
-    setForm({ content: '' })
   }
 
   return (
--- src/components/NewQuestion/NewQuestion.jsx
+++ src/components/NewQuestion/NewQuestion.jsx
@@ -12,15 +12,79 @@ const NewQuestion = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    props.handleAddQuestion(form)
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-
+        <form onSubmit={handleSubmit}>
+          <input
+            required
+            autoComplete="off"
+            type="text"
+            name="name"
+            value={form.title}
+            placeholder="Add a title"
+            onChange={handleChange}
+          />
+        <textarea
+          type="text"
+          name="name"
+          value={form.title}
+          placeholder="Add optional body text"
+          onChange={handleChange}
+        />
+        <button type="submit">Create Question</button>
       </form>
     </div>
   )
 }
 
 export default NewQuestion
+
+
+//   return (
+//     <main className={styles.container}>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="title-input">Title</label>
+//         <input
+//           required
+//           type="text"
+//           name="title"
+//           id="title-input"
+//           value={form.title}
+//           placeholder="Title"
+//           onChange={handleChange}
+//         />
+//         <label htmlFor="text-input">Text</label>
+// 				<textarea
+//           required
+//           type="text"
+//           name="text"
+//           id="text-input"
+//           value={form.text}
+//           placeholder="Text"
+//           onChange={handleChange}
+//         />
+//         <label htmlFor="category-input">Category</label>
+//         <select
+//           required
+//           name="category"
+//           id="category-input"
+//           value={form.category}
+//           onChange={handleChange}
+//         >
+//           <option value="News">News</option>
+//           <option value="Games">Games</option>
+//           <option value="Music">Music</option>
+//           <option value="Movies">Movies</option>
+//           <option value="Sports">Sports</option>
+//           <option value="Television">Television</option>
+//         </select>
+//         <button type="submit">SUBMIT</button>
+//       </form>
+//     </main>
+//   )
+// }
+
+// export default NewBlog
