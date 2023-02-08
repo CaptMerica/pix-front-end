bf302635c72be4eadcd5b302f7051fa65282717c
Oscar Nunez <requinto31@gmail.com>
2023-02-08 16:22:15 -0500

    apply styling buttons for logged in users

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,31 +1,47 @@
 import { Link } from 'react-router-dom'
 import './NavBar.css'
+import logo from '../../../src/assets/teal-pick.png'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <div className='navbar'>
       <div className='icon-div'>
-        <h1>ICON</h1>
+        <img id='logo'src={logo} alt='teal-pick' />
       </div>
       <div className='button-div'>
         {user ?
           <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/questions">Questions</Link></li>
-            <li><Link to="/questions/new">New Question</Link></li>
-            <li><Link to="quotes">Quotes</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-          </ul>
-        :
-          <ul>
+            <li>
+              <Link to="/questions">
+                <button className='nav-btn'>Questions</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/questions/new">
+                <button className='nav-btn'>New Questions</button>
+              </Link>
+            </li>
             <li>
               <Link to="quotes">
                 <button className='nav-btn'>Quotes</button>
               </Link>
             </li>
             <li>
+              <Link to="" onClick={handleLogout}>
+                <button className='nav-btn'>Log Out</button>
+              </Link>
+            </li>
+          </ul>
+        :
+          <ul>
+            <li>
               <Link to="/questions">
                 <button className='nav-btn'>Questions</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="quotes">
+                <button className='nav-btn'>Quotes</button>
               </Link>
             </li>
             <li>
