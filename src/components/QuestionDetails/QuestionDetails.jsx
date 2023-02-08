54e9bcdc6dba225e36a5c2bb21a0829b6926e4bf
CaptMerica <rmerica@live.com>
2023-02-08 10:28:21 -0500

    update delete comment functionality

--- src/components/QuestionDetails/QuestionDetails.jsx
+++ src/components/QuestionDetails/QuestionDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { Link } from "react-router-dom"
-import { show } from "../../services/questionService"
+import { deleteComment, show } from "../../services/questionService"
 import { createComment } from "../../services/questionService"
 import NewComment from "../NewComment/NewComment"
 
@@ -12,6 +12,11 @@ const QuestionDetails = (props) => {
   const handleAddComment = async (commentData) => {
     const newComment = await createComment(id, commentData)
     setQuestionDetails({ ...questionDetails, comments: [...questionDetails.comments, newComment] })
+  }
+
+  const handleDeleteComment = async (questionId, commentId) => {
+    const deletedComment = await deleteComment(questionId, commentId)
+    setQuestionDetails({ ...questionDetails, comments: questionDetails.comments.filter((c) => c._id !== deletedComment._id) })
   }
 
   useEffect(() => {
@@ -61,6 +66,7 @@ const QuestionDetails = (props) => {
           {questionDetails.comments.map((comment) => (
             <li key={comment._id}>
               {comment.content}
+              <button onClick={() => handleDeleteComment(questionDetails._id, comment._id)}>Delete Comment</button>
             </li>
           ))}
         </ul>
