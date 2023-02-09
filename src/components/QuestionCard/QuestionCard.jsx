5c0891d4100a632c6347bc32802b18adfade4840
Patrick Mazara <patrickmazara@hotmail.com>
2023-02-09 13:28:42 -0500

    move location of Link element to QuestionsList component, style divs appropriately

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,24 +1,22 @@
 import '../QuestionCard/QuestionCard.css'
-import { Link } from "react-router-dom"
-
 
 const QuestionCard = ({question}) => {
   const photo = question.owner.photo
 
   return (
     <>
-      <Link to={`/questions/${question._id}`}>
         <div className="card-photo-div">
           <img className="card-photo-img" src={photo} alt="ownerPhoto" />
         </div>
-        <div>
-          <h2>{question.owner.name}</h2>
-          <h2>{question.name}</h2>
+        <div className='card-properties'>
+          {/* <div>
+            <h2>{question.owner.name}</h2>
+            <h2>{question.name}</h2>
+          </div> */}
+          <div>
+            <h5>{question.content}</h5>
+          </div>
         </div>
-        <div>
-          <h5>{question.content}</h5>
-        </div>
-      </Link >
     </>
   )
 }
--- src/pages/QuestionsList/QuestionList.jsx
+++ src/pages/QuestionsList/QuestionList.jsx
@@ -1,15 +1,18 @@
 import '../QuestionsList/QuestionsList.css'
 import QuestionCard from "../../components/QuestionCard/QuestionCard"
+import { Link } from 'react-router-dom'
 
 const QuestionsList = (props) => {
   return (
     <section className="questions-list">
       {props.questions.map((question, idx) => (
-        <div className="question-card" key={idx}>
+        <Link key={idx} to={`/questions/${question._id}`}>
+        <div className="question-card">
           <QuestionCard 
             question={question}
           />
         </div>
+        </Link>
       ))}
     </section>
   )
