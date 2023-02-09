c9650ad1e1bb5cac971d253de01c5c031eb4ba70
Patrick Mazara <patrickmazara@hotmail.com>
2023-02-09 10:50:38 -0500

    create div for QuestionCard image

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,10 +1,16 @@
+import '../QuestionCard/QuestionCard.css'
 import { Link } from "react-router-dom"
 
 
 const QuestionCard = ({question}) => {
+  const photo = question.owner.photo
+
   return (
     <>
       <Link to={`/questions/${question._id}`}>
+        <div className="card-photo-div">
+          <img className="card-photo-img" src={photo} alt="ownerPhoto" />
+        </div>
         <div>
           <h2>{question.owner.name}</h2>
           <h2>{question.name}</h2>
