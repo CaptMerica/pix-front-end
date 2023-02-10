b506049013cd13fa8bff9fe0e68c63466e033cd9
Oscar Nunez <requinto31@gmail.com>
2023-02-09 23:11:40 -0500

    apply sizing to landing app name and change background

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,14 @@
 import styles from './Landing.module.css'
+import logo from '../../assets/teal-pick-larger.png'
 
 const Landing = ({ user }) => {
   return (
-    <>
-      <main className={styles.container}>
-        <h1>hello, {user ? user.name : 'friend'}</h1>
-      </main>
-    </>
+    <main className={styles.container}>
+        <h1 className={styles.landingTitle}>PIX</h1>
+      <div className={styles.landingBackground}>
+        <img src={logo} alt="large-teal-pick" />
+      </div>
+    </main>
   )
 }
 
