4f0612cbdff0f7b793e9b1745eb988a37ec43606
CaptMerica <rmerica@live.com>
2023-02-11 18:40:33 -0500

    remove assets folder, define variables for image links

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,14 @@
 import styles from './Landing.module.css'
-import logo from '../../assets/teal-pick-larger.png'
 
 const Landing = ({ user }) => {
+
+  const landingPhoto = 'https://i.imgur.com/CZoAtq4.png'
+
   return (
     <main className={styles.container}>
         <h1 className={styles.landingTitle}>PIX</h1>
       <div className={styles.landingBackground}>
-        <img src={logo} alt="large-teal-pick" />
+        <img src={landingPhoto} alt='landing' />
       </div>
     </main>
   )
